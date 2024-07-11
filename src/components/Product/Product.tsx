5d0429c3defe670b24e7075d5bff7080b0f7e878
MauriDiPietro <dipietro.jm@gmail.com>
2024-07-11 01:23:15 -0300

    fix

--- src/components/Product/Product.tsx
+++ src/components/Product/Product.tsx
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from "react";
 
-import { initMercadoPago, Wallet } from "@mercadopago/sdk-react";
+import { initMercadoPago, 
+  // Wallet
+ } from "@mercadopago/sdk-react";
 
 export const Product: React.FC = () => {
   initMercadoPago("APP_USR-f246badc-7f43-4d1c-b9e2-f9252d1cfa9f", {
@@ -8,7 +10,7 @@ export const Product: React.FC = () => {
   });
 
   const [products, setProducts] = useState<any[]>([]);
-  const [preferenceIds, setPreferenceIds] = useState(null);
+  // const [preferenceIds, setPreferenceIds] = useState(null);
 
 
   //https://mockbin.io
@@ -32,26 +34,26 @@ export const Product: React.FC = () => {
     }
   };
 
-  const createPreference = async (prod: any) => {
-    try {
-        const body = {
-            title: prod.name,
-            quantity: 1,
-            price: prod.price
-        }
-        const res = await fetch("http://localhost:8080/create-preference", {
-            method: "POST",
-            body: JSON.stringify(body),
-            headers:{'Content-Type': 'application/json'}
-        })
-        const response = await res.json();
-        console.log('response api', response);
-        const { id } = response;
-        return id;
-    } catch (error) {
-        console.log(error);
-    }
-  }
+  // const createPreference = async (prod: any) => {
+  //   try {
+  //       const body = {
+  //           title: prod.name,
+  //           quantity: 1,
+  //           price: prod.price
+  //       }
+  //       const res = await fetch("http://localhost:8080/create-preference", {
+  //           method: "POST",
+  //           body: JSON.stringify(body),
+  //           headers:{'Content-Type': 'application/json'}
+  //       })
+  //       const response = await res.json();
+  //       console.log('response api', response);
+  //       const { id } = response;
+  //       return id;
+  //   } catch (error) {
+  //       console.log(error);
+  //   }
+  // }
   //! éste metodo tenemos que aplicarselo a todos los productos.. como hacemos?
   //!--> updatePreferenceIds
 
@@ -61,17 +63,17 @@ export const Product: React.FC = () => {
 
   useEffect(() => {
     const updatePreferenceIds = async () => {
-      const ids = {};
+      // const ids = {};
       //va a recorrer el array de products
-      for (const prod of products) {
+      // for (const prod of products) {
         //por cada uno va a llamar al meodo createPreference
-        const preferenceId = await createPreference(prod);
+        // const preferenceId = await createPreference(prod);
         //y va a agregar en el objeto como propiedad el id del producto y como valor el preferenceId que 
         //generó el servidor.
         //{1: "sdfsfdf", 2: "dsfsdfdgggg"}
         // ids[prod.id] = preferenceId;
-        console.log('ids', ids);
-      }
+      //   console.log('ids', ids);
+      // }
       // setPreferenceIds(ids);
     };
 
@@ -98,9 +100,9 @@ export const Product: React.FC = () => {
               <h3>{prod.name}</h3>
               <p>${prod.price}</p>
               {
-            preferenceIds &&    
-              <Wallet
-                initialization={{ preferenceId: preferenceIds[prod.id]  }}    //datos del producto que van al servidor
+            // preferenceIds &&    
+              // <Wallet
+                // initialization={{ preferenceId: preferenceIds[prod.id]  }}    //datos del producto que van al servidor
                                                                         //nuestro servidor va a recibir estos datos
                                                                         //y nos va a devolver un id,
                                                                         //para eso vamos a crear un endpoint en el back
@@ -108,8 +110,8 @@ export const Product: React.FC = () => {
                                                                         //desde aca llamamos a ese post y le pasamos los 
                                                                         //datos del producto
                                                                         //!--> createPreference
-                customization={{ texts: { valueProp: "smart_option" } }}
-              />
+                // customization={{ texts: { valueProp: "smart_option" } }}
+              // />
               }
             </>
           );
